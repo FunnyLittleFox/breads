0f353e3b8b0c4ea204153e9e31bcd7e9b993eebb
Fox Flaherty <katyra14@gmail.com>
2022-06-20 06:40:28 -0700

    sending index and show data

--- server.js
+++ server.js
@@ -2,13 +2,19 @@
 const { application } = require('express')
 const express = require('express')
 const app = express ()
+
 //CONFIGURATION
 require('dotenv').config()
 const PORT = process.env.PORT
 console.log(PORT)
 
+//MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 //ROUTES
-application.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads!')
 })
 
